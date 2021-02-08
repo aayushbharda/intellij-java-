3e579b26493173ce9e72e30111ab5f742bc9ebcf
aayush <aayushbhardwaj92@gmail.com>
2021-02-08 17:59:54 +0530

    can add and search word and paragraph. can add the paragraph is which the word is to be search.

---
+++ com/company/MatchFound.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MatchFound {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("write a word to search");
+        String s = sc.nextLine();
+        System.out.println("paste the paragraph to search the word");
+        String t =sc.nextLine();
+        Pattern pattern = Pattern.compile(s, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(t);
+        boolean matchFound = matcher.find();
+        if(matchFound) {
+            System.out.println("Match found");
+        } else {
+            System.out.println("Match not found");
+        }
+    }
+}
+
