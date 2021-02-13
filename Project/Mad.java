a2808939e9de856d9d1106cb34ff58206e5a9a7f
aayush <aayushbhardwaj92@gmail.com>
2021-02-13 23:53:45 +0530

    new class mad

---
+++ Project/Mad.java
@@ -0,0 +1,22 @@
+package Project;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Mad {
+    Scanner sc =new Scanner(System.in);
+    String story;
+    String name;
+    String adjective1;
+    String adjective2;
+    String noun1;
+    String noun2;
+    String noun3;
+    String  adverb;
+    String  randomNum;
+     Random rand=new Random();
+
+    public static void main(String[] args) {
+
+    }
+}
