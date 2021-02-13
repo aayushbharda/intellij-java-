b7c0d83b64c8e20d753f93d3342be91892ce804f
aayush <aayushbhardwaj92@gmail.com>
2021-02-13 15:26:38 +0530

    new

--- com/company/StringFunction.java
+++ com/company/StringFunction.java
@@ -1,6 +1,8 @@
 package com.company;
 
 
+import com.sun.security.jgss.GSSUtil;
+
 import java.util.Scanner;
 
 public class StringFunction {
@@ -8,35 +10,18 @@ public class StringFunction {
 
         Scanner sc= new Scanner(System.in);
         String s = sc.nextLine();
-
-        System.out.println(s);
-
-        System.out.println(s.length());
+        String q = sc.nextLine();
+        String w = sc.nextLine();
 
         System.out.println(s.toLowerCase());
+        System.out.println(s.replace(" ","_"));
 
-        System.out.println(s.toUpperCase());
-        String e ="     canopy       ";
-        System.out.println(e.trim());
+        String le="Dear <|name|>,Thanks a lot";
+        System.out.println(le.replace("<|name|>",w));
 
+        //detect double or triple space
 
-        System.out.println(s.substring(7 ));
-        System.out.println(s.substring(6,10));
-
-        String d =sc.nextLine();
-        String f = sc.nextLine();
-        String g = sc.nextLine();
-        System.out.println(s.replace(d,f));
-
-        System.out.println(s.startsWith(g));
-        System.out.println(s.endsWith(g));
-
-        System.out.println(s.charAt(6));
-        System.out.println(s.indexOf(4));
-        System.out.println(s.indexOf('y'));
-        System.out.println(s.lastIndexOf("awadadash",3));
-
-
+        System.out.println(s.indexOf("  "));
 
 
     }
